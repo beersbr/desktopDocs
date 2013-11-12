5c0726ea5193e89bbe3fa944c73fa3c3736cbf36
Mramaizng <beersbr@gmail.com>
2013-11-11 17:26:05 -0800

    removed the fielsystem aware code. You just pass an input and optional output file path to it now

--- desktop_docs.rb
+++ desktop_docs.rb
@@ -1,12 +1,36 @@
 require 'redcarpet'
 require 'date'
 
-puts "Remember to put all your markdown in a markdown folder, named with camel notation. The ruby script will do the rest"
+STYLES = <<-STYLE
+<style>
+body{
+	font-family: verdana, sans-serif;
+	font-size: 14px;
+}
 
-HEADER = lambda {|title| "<!doctype html><html><head><link rel=Stylesheet href=\"../docstyle.css\" media=screen><title>#{title}</title></head><body><div><sub>[<a href=\"javascript:window.history.back();\">Back</a>]</sub></div>" }
+code{
+	white-space: pre;
+}
+</style>
+STYLE
+
+HEADER = lambda {|title| "<!doctype html><html><head>#{STYLES}<title>#{title}</title></head><body><div><sub>[<a href=\"javascript:window.history.back();\">Back</a>]</sub></div>" }
 FOOTER = "<div><sub>[<a href=\"javascript:window.history.back();\">Back</a>]</sub></div><body></html>"
 
-outputname = "index.html"
+inputFile = ARGV[0]
+outputFile = ARGV[1] 
+
+if not File.exists? inputFile
+	throw "Could not find file: #{inputFile}"
+end
+
+
+if outputFile.nil?
+	outputname = "#{inputFile}.html"
+else
+	outputname = outputFile
+end
+
 
 # create the docs directory if it doesnt already exist
 Dir.mkdir "docs" if not(Dir.exists? "docs")
@@ -16,8 +40,7 @@ Dir.mkdir "markdown" if not (Dir.exists? "markdown")
 File.unlink "./index.html" if (File.exists? "./index.html")
 
 # create the generator
-HtmlGen = Redcarpet::Markdown.new(Redcarpet::Render::HTML, :autolink => true, :space_after_headers => true)
-
+HtmlGen = Redcarpet::Markdown.new(Redcarpet::Render::HTML, :autolink => true, :space_after_headers => true, :forced_code_blocks => true)
 
 File.open("index.html", "w+"){ |html|
 	html.puts "<!doctype html>"
@@ -26,29 +49,21 @@ File.open("index.html", "w+"){ |html|
 	html.puts "<sub>Updated at #{DateTime.now.to_s}</sub>"
 	html.puts "<div><ul>"
 
-	Dir.foreach("./markdown/"){ |link|
-		next if not(link =~ /^([a-zA-Z0-9]*)\.md$/)
-		filename = $~[1]
+	puts "Generating #{inputFile}"
 
-		puts "Generating #{filename}"
+	File.open("#{outputname}", "w+"){ |fd|
+		File.open("#{inputFile}", "r"){ |fr|
+			fd.puts HEADER.call(inputFile)
+			fdata = ""
+			while(line = fr.gets)
+				fdata += line
+				# fd.puts(HtmlGen.render(line))
+			end
 
-		html.puts "<li><a href=\"docs\\#{$~[1]}.html\">#{($~[1]).gsub(/([a-z])([A-Z])/, "\\1 \\2")}</a></li>"
-
-		File.open("./docs/#{filename}.html", "w+"){ |fd|
-			File.open("./markdown/#{filename}.md", "r"){ |fr|
-				fd.puts HEADER.call(filename)
-				fdata = ""
-				while(line = fr.gets)
-					fdata += line
-					# fd.puts(HtmlGen.render(line))
-				end
-
-				fd.puts(HtmlGen.render(fdata))
-			}
-
-			fd.puts FOOTER
+			fd.puts(HtmlGen.render(fdata))
 		}
-	
+
+		fd.puts FOOTER
 	}
 
 	html.puts "</ul></div></body></html>"
